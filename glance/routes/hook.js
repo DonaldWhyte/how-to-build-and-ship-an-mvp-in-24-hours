8fa0c3631fcc956d21d57d2a6e737a64b27826a4
Mansimran Singh <me@mansimransingh.com>
2016-08-10 18:37:27 +0100

    maybe its GET?

--- glance/routes/hook.js
+++ glance/routes/hook.js
@@ -8,6 +8,20 @@ var User = require('../models/User');
 
 var Trello = require('../engines/trello');
 
+router.get('/:id', function(req, res, next){
+    Hook.findOne({ _id: req.params.id})
+        .exec(function(err, doc){
+            if (err) return next(err);
+
+            doc.meta = req.body;
+            doc.save(function(err, savedDoc){
+                if (err) return next(err);
+
+                res.json({success:true});
+            }); 
+        }); 
+});
+
 router.post('/:id', function(req, res, next) {
     Hook.findOne({ _id: req.params.id})
         .exec(function(err, doc){
